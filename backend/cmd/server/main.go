b6e3f1ad163d9dee26fae2c3b1570d01e8718367
Dmitry Vasilev <mekon26@gmail.com>
2026-04-25 22:53:48 +0300

    Add Docker deployment setup

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -4,21 +4,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"naberi-backend/internal/naberi"
 )
 
 func main() {
-	addr := os.Getenv("NABERI_ADDR")
-	if addr == "" {
-		addr = ":4010"
-	}
+	addr := listenAddress()
 
 	server := naberi.NewServer(naberi.NewStore(), time.Local)
+	handler := server.Routes()
+
+	if staticDir := os.Getenv("NABERI_WEB_DIR"); staticDir != "" {
+		handler = naberi.WithStaticFiles(handler, staticDir)
+	}
 
 	log.Printf("Naberi backend listening on %s", addr)
-	if err := http.ListenAndServe(addr, server.Routes()); err != nil {
+	if err := http.ListenAndServe(addr, handler); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func listenAddress() string {
+	if addr := os.Getenv("NABERI_ADDR"); addr != "" {
+		return addr
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		return ":4010"
+	}
+
+	if strings.HasPrefix(port, ":") {
+		return port
+	}
+
+	return ":" + port
 }
